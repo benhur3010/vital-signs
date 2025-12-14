0cae2bd7fce0be3daae22ed8a5d860948187b5aa
Ben-Hur Bueno <benhurbueno.dev@gmail.com>
2025-12-13 21:22:31 -0300

    feat: responsive layout in banner home

--- app/components/home/BannerHome.tsx
+++ app/components/home/BannerHome.tsx
@@ -2,32 +2,52 @@ import Image from "next/image";
 
 export default function BannerHome() {
   return (
-    <div className=" bg-span pt-5">
-      <div className="relative min-h-205 bg-[url('/banner-home/banner-home.png')] bg-cover bg-no-repeat">
-        <div className="absolute space-y-3 max-w-35 -translate-y-1/2 top-[47%] rounded-2xl p-4 left-[66%] z-10 bg-[rgba(255,255,255,0.22)] border border-light shadow-[0_3px_6px_#00000029] backdrop-blur-[17px] backdrop-brightness-[1.24]">
-          <div className="grid grid-cols-12 gap-3">
-            <span className="font-bold text-primary col-span-4">SYS</span>{" "}
-            <span className="col-span-8">140</span>
-          </div>
-          <div className="grid grid-cols-12 gap-3">
-            <span className="font-bold text-primary col-span-4">DIA</span>{" "}
-            <span className="col-span-8">82</span>
-          </div>
-          <div className="grid grid-cols-12 gap-3">
-            <span className="col-span-4">
-              <Image
-                src="/banner-home/heart.png"
-                width={20}
-                height={20}
-                alt="coração"
-              />
-            </span>{" "}
-            <span className="col-span-8">100bpm</span>
+    <div className="bg-span pt-5">
+      <div className="relative w-full">
+        <div className="relative w-full aspect-1920/820 2xl:aspect-1920/820">
+          <Image
+            src="/banner-home/banner-home.png"
+            alt="Banner Vital Signs"
+            fill
+            priority
+            sizes="100vw"
+            className="object-cover object-center"
+          />
+
+          <div
+            className="
+              absolute z-10
+              left-[70%] top-[47%] -translate-x-1/2 -translate-y-1/2
+              max-w-35 space-y-3 rounded-2xl p-4
+              bg-[rgba(255,255,255,0.22)] border border-light
+              shadow-[0_3px_6px_#00000029]
+              backdrop-blur-[17px] backdrop-brightness-[1.24]
+            "
+          >
+            <div className="grid grid-cols-12 gap-3">
+              <span className="font-bold text-primary col-span-4">SYS</span>
+              <span className="col-span-8">140</span>
+            </div>
+            <div className="grid grid-cols-12 gap-3">
+              <span className="font-bold text-primary col-span-4">DIA</span>
+              <span className="col-span-8">82</span>
+            </div>
+            <div className="grid grid-cols-12 gap-3">
+              <span className="col-span-4">
+                <Image
+                  src="/banner-home/heart.png"
+                  width={20}
+                  height={20}
+                  alt="coração"
+                />
+              </span>
+              <span className="col-span-8">100bpm</span>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="container text-center py-12">
+      <div className="container text-center py-12 px-5">
         {" "}
         <h5 className="max-w-240 font-light mx-auto text-primary">
           {" "}
@@ -40,7 +60,7 @@ export default function BannerHome() {
         </h5>
       </div>
 
-      <div className="bg-primary py-8">
+      <div className="bg-primary py-8 px-5">
         <div className="max-w-160 mx-auto text-center">
           <h2 className="text-span font-light leading-tight">
             Feito {/* eslint-disable-next-line @next/next/no-img-element */}
