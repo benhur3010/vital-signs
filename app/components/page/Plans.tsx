cf398d813914df39fc7a17e89c96f3aacaaad505
Ben-Hur Bueno <benhurbueno.dev@gmail.com>
2025-12-23 22:59:24 -0300

    feat: cards in plans component

--- app/components/page/Plans.tsx
+++ app/components/page/Plans.tsx
@@ -16,7 +16,7 @@ export default function Plans() {
         "
       />
 
-      <div className="container pt-19 text-center">
+      <div className="max-w-230 mx-auto pt-19 text-center">
         <h2>
           Invista na {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
@@ -33,6 +33,77 @@ export default function Plans() {
           />{" "}
           sua saúde!
         </h2>
+
+        <div className="flex justify-between items-center text-start mt-25 min-h-125 text-secondary">
+          <div className="w-105 pt-8 px-10 pb-11 bg-white rounded-2xl space-y-5">
+            <h4 className="text-primary font-bold">PLANO FREE</h4>
+            <p>
+              Acompanhe seus sinais vitais e histórico de saúde de forma
+              independente, com acesso ao{" "}
+              <span className="font-bold">VitalScore™ básico.</span>
+            </p>
+            <h4 className="text-primary font-extrabold">Grátis</h4>
+            <ul className="space-y-2">
+              {[
+                "Monitoramento de sinais vitais",
+                "Histórico de saúde pessoal",
+                "VitalScoreTM básico",
+              ].map((item) => (
+                <li
+                  key={item}
+                  className="relative pl-5 before:absolute before:left-0 before:top-0 before:content-['✓'] before:text-primary before:font-bold"
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+            <button className="mt-3 border border-primary text-primary rounded-full hover:cursor-pointer hover:bg-primary hover:text-white transition duration-300 py-1 px-3">
+              Baixe o App
+              <span className="ml-2">→</span>
+            </button>
+          </div>
+
+          <div className="w-115 pt-8 px-10 pb-4 bg-accent rounded-2xl space-y-5 relative">
+            <h4 className="text-primary font-bold">PLANO ASSINATURA</h4>
+            <p>
+              Receba{" "}
+              <span className="font-bold">
+                acompanhamento por profissionais de saúde,
+              </span>{" "}
+              alertas avançados e relatórios preditivos{" "}
+              <span className="font-bold">integrados ao seu médico</span>
+              ou plano de saúde.
+            </p>
+            <h4 className="text-primary font-extrabold">R$ 200/mês</h4>
+            <ul className="space-y-2">
+              {[
+                "Tudo do Plano Free",
+                "Acompanhamento por profissionais de saúde",
+                "Alertas avançados",
+                "Relatórios preditivos completos",
+                "Integração com seu médico/plano de saúde",
+              ].map((item) => (
+                <li
+                  key={item}
+                  className="relative pl-5 before:absolute before:left-0 before:top-0 before:content-['✓'] before:text-primary before:font-bold"
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+            <button className="mt-3 text-white bg-primary hover:bg-secondary rounded-full hover:cursor-pointer transition duration-300 py-1.5 px-3">
+              Baixe o App
+              <span className="ml-2">→</span>
+            </button>
+
+            <div className="absolute bg-span fitgree py-1 px-5 rounded-full -top-5 right-0">
+              RECOMENDADO
+            </div>
+            <div className="absolute bg-blue-light text-primary fitgree py-1 px-5 rounded-full -bottom-7 right-8">
+              Em breve integração com planos e operadoras
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
